6060ac777f92128b95e821dcd7aa8249653c6031
Shumyk <dima.shumyk@gmail.com>
2022-01-30 12:59:44 +0200

    [35] java 8: method reference

--- src/rocks/shumyk/functional/method/reference34/MethodReferenceExample.java
+++ src/rocks/shumyk/functional/method/reference34/MethodReferenceExample.java
@@ -1,6 +1,7 @@
 package rocks.shumyk.functional.method.reference34;
 
 import rocks.shumyk.functional.mocked.db.Instructor;
+import rocks.shumyk.functional.mocked.db.Instructors;
 
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -12,5 +13,15 @@ public class MethodReferenceExample {
 		Function<Integer, Double> sqrt = Math::sqrt;
 
 		Function<String, String> lowercaseFunc = String::toLowerCase;
+
+		Predicate<Instructor> years = MethodReferenceExample::greaterThanTenYearsExperience;
+
+		Instructors.getAll().stream()
+			.filter(years)
+			.forEach(System.out::println);
+	}
+
+	public static boolean greaterThanTenYearsExperience(Instructor instructor) {
+		 return instructor.getYearsOfExperience() > 10;
 	}
 }
